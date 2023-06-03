b329a20b79ea4a22cd007656f0f7a3af76baba8c
张雷 <3537168410@qq.com>
2023-06-03 14:58:27 +0800

    去除base_url

--- vue-web/vue.config.js
+++ vue-web/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'?'/DataLife/':'',
+    // publicPath: process.env.NODE_ENV === 'production'?'/DataLife/':'',
     configureWebpack:{
         module: {
             rules: [
